6c0653ee4b545f850f85c4409f196e16732340f4
devKarthikRaj <rocketraj2000@gmail.com>
2021-08-24 16:52:52 +0800

    Notifications can now be sent in All Things Notifications

---
+++ AllThingsNotifications/app/src/main/java/com/raj/allthingsnotifications/BaseApp.java
@@ -0,0 +1,32 @@
+package com.raj.allthingsnotifications;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class BaseApp extends Application {
+    public final static String CHANNEL_1_ID = "channel1";
+    public final static String CHANNEL_2_ID = "channel2";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannels();
+    }
+
+    private void createNotificationChannels() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(CHANNEL_1_ID,"Channel 1",NotificationManager.IMPORTANCE_HIGH);
+            channel1.setDescription("This is notification channel 1");
+
+            NotificationChannel channel2 = new NotificationChannel(CHANNEL_2_ID,"Channel 2",NotificationManager.IMPORTANCE_LOW);
+            channel1.setDescription("This is notification channel 2");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel1);
+            notificationManager.createNotificationChannel(channel2);
+        }
+    }
+}
---
+++ AllThingsNotifications/app/src/main/java/com/raj/allthingsnotifications/MainActivity.java
@@ -0,0 +1,80 @@
+package com.raj.allthingsnotifications;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static com.raj.allthingsnotifications.BaseApp.CHANNEL_1_ID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private NotificationManagerCompat notificationManagerCompat;
+    private EditText etNotifTitle;
+    private EditText etNotifDesc;
+    private Button btnSendNotifCh1;
+    private Button btnSendNotifCh2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notificationManagerCompat = notificationManagerCompat.from(this);
+
+        etNotifTitle = findViewById(R.id.ET_Notif_Title);
+        etNotifDesc = findViewById(R.id.ET_Notif_Desc);
+        btnSendNotifCh1 = findViewById(R.id.BTN_Send_On_Ch1);
+        btnSendNotifCh2 = findViewById(R.id.BTN_Send_On_Ch2);
+
+        btnSendNotifCh1.setOnClickListener(this);
+        btnSendNotifCh2.setOnClickListener(this);
+    }
+
+    public void sendOnChannel1(View v) {
+        String notifTitle = etNotifTitle.getText().toString();
+        String notifDesc = etNotifDesc.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(notifTitle)
+                .setContentText(notifDesc)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManagerCompat.notify(1, notification);
+    }
+
+    public void sendOnChannel2(View v) {
+        String notifTitle = etNotifTitle.getText().toString();
+        String notifDesc = etNotifDesc.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(notifTitle)
+                .setContentText(notifDesc)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+
+        notificationManagerCompat.notify(2, notification);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.BTN_Send_On_Ch1:
+                sendOnChannel1(view);
+                break;
+            case R.id.BTN_Send_On_Ch2:
+                sendOnChannel2(view);
+                break;
+        }
+    }
+}
